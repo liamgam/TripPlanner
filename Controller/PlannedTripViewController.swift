800e4af0b454106e36954ab50f70a9f3e0c7f5bd
liamgam <liam.liamgamble@gmail.com>
2019-04-26 13:30:27 -0700

    added starter code with tableview showing mock detail

---
+++ Controller/PlannedTripViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlannedTripViewController.swift
+//  TripPlanner
+//
+//  Created by Rinni Swift on 4/26/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class PlannedTripViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var tableview: UITableView!
+    
+    // MARK: - Variables
+    var trips = ["Monterey", "Paris", "Tokyo", "San Francisco", "New York"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableview.dataSource = self
+        tableview.delegate = self
+        
+    }
+}
+
+extension PlannedTripViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trips.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "tripCell") as! TripTableViewCell
+        cell.tripNameLabel.text = trips[indexPath.row]
+        return cell
+    }
+}
+
---
+++ View/TripTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TripTableViewCell.swift
+//  TripPlanner
+//
+//  Created by Rinni Swift on 4/26/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tripNameLabel: UILabel!
+    
+}
