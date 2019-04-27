0664168cf4b92eb40354e1d8d56430b61eefdb93
liamgam <liam.liamgamble@gmail.com>
2019-04-27 11:03:26 -0700

    added core data helper class to use in all classes

--- Model/CoreDataHelper.swift
+++ Model/CoreDataHelper.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataHelper {
+
+    var managedObjectContext = AppDelegate.context
+    
+    func fectchAll() -> [Trip]? {
+        let request: NSFetchRequest<Trip> = Trip.fetchRequest()
+        do {
+            let results = try managedObjectContext.fetch(request)
+            return results
+        } catch let error as NSError {
+            print(error, error.userInfo)
+        }
+        return nil
+    }
+
+    func saveContext() {
+        do {
+            try managedObjectContext.save()
+        } catch let error as NSError {
+            print(error, error.userInfo)
+        }
+    }
+}
