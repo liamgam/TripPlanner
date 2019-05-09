619027f649902e5fc35a61b31905f0d47adb3611
liamgam <liam.liamgamble@gmail.com>
2019-05-08 19:15:16 -0700

    added git ignore file

---
+++ TripPlanner/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TripPlanner
+//
+//  Created by Rinni Swift on 5/8/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+
+let GOOGLE_API_KEY = ""
