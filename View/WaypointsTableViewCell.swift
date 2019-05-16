3c35a9f655feaadd5686422b6ffd9df14a5b5379
liamgam <liam.liamgamble@gmail.com>
2019-05-15 19:20:21 -0700

    added waypoints cell

---
+++ View/WaypointsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WaypointsTableViewCell.swift
+//  TripPlanner
+//
+//  Created by Rinni Swift on 5/15/19.
+//  Copyright © 2019 Rinni Swift. All rights reserved.
+//
+
+import UIKit
+
+class WaypointsTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var waypointLabel: UILabel!
+
+}
