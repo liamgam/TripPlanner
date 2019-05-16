78545d38b0d15013e57325a6e2f43ab04123f486
liamgam <liam.liamgamble@gmail.com>
2019-05-15 20:40:24 -0700

    displays waypoints on tableView

--- Controller/AddWaypointsViewController.swift
+++ Controller/AddWaypointsViewController.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class AddWaypointsViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    var trip: Trip?
+    var waypoints: NSMutableOrderedSet?
+    
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBAction func addMoreButtonTapped(_ sender: UIButton) {
         if let mapVC = navigationController?.storyboard?.instantiateViewController(withIdentifier: "mapVC") as? MapViewController {
+            mapVC.trip = trip
             navigationController?.pushViewController(mapVC, animated: true)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
+        
+        waypoints = trip?.waypoint
     }
+}
+
+extension AddWaypointsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if waypoints == nil {
+            return 0
+        } else {
+            return waypoints!.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "waypointCell") as! WaypointsTableViewCell
+        if waypoints != nil {
+            let waypoint = waypoints![indexPath.row] as! Waypoint
+            cell.waypointLabel.text = waypoint.name
+        }
+        return cell
+    }
+    
 }
