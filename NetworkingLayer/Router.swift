b0ef0e84bc915aef4b29002968b50dc89cb242af
liamgam <liam.liamgamble@gmail.com>
2019-05-16 18:38:47 -0700

    added router class

--- NetworkingLayer/Router.swift
+++ NetworkingLayer/Router.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+enum Router {
+    case fetchName(name: String)
+
+    var scheme: String {
+        switch self {
+        case .fetchName(let name):
+            return "https"
+        }
+    }
+    
+    var host: String {
+        switch self {
+        case .fetchName(let name):
+            return "maps.googleapis.com"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchName(let name):
+            return "/maps/api/place/textsearch/json"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        switch self {
+        case .fetchName(let name):
+            return [URLQueryItem(name: "query", value: name),
+                    URLQueryItem(name: "key", value: GOOGLE_API_KEY)]
+        }
+    }
+}
